e99a38d1c1ce5ef70c0c63407a33f89b0c5808cd
devops0819 <163329072+devops0819@users.noreply.github.com>
2024-03-29 20:29:38 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,13 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    status_code = random.choice([200, 200, 200, 404, 500])
+    return 'Hello, World!', status_code
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
